979217469e9ad62b9de053136df76d0c3917eaf2
SrtaK <atorrecillam@gmail.com>
2023-05-10 17:42:53 +0200

    custome hook

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,25 +1,9 @@
 
-import { useEffect, useState } from 'react';
-
 import { GifItem } from './GifItem';
-import { getGifs } from '../helpers/getGifs';
+import { useFetchGifs } from '../hooks/useFetchGifs';
 
 export const GifGrid = ({category}) => {
-  //en un funcional component como este NO tendríamos que colocar funciones
-  //porque cada vez que se realiza un cambio, se lee de nuevo y se llama a la función
-  //no queremos volver a hacer una petición de fetch
-
-  const [images, setImages] = useState([]);
-
-  const getImages = async() => {
-    const newImages = await getGifs( category );
-    setImages(newImages)
-  }
-  
-  useEffect(()=> {
-    getImages();
-  }, [])
-
+  const { images, isLoading } = useFetchGifs( category );
 
   return (
     <>
--- src/components/GifItem.jsx
+++ src/components/GifItem.jsx
@@ -1,5 +1,5 @@
 export const GifItem = ({ title, url }) => {
-  console.log(title, url)
+  
   return (
     <div className="card">
       <img src={ url } alt={ title } />
